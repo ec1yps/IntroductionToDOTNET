d57518eaddc18de152da66a4d49798e88041a7fe
ec1yps_ <Mihli_sk06@top-academy.ru>
2024-10-01 17:37:58 +0300

    Homework DONE

--- IntroductionToDOTNET/Program.cs
+++ IntroductionToDOTNET/Program.cs
@@ -1,5 +1,9 @@
 ﻿//#define CONSOLE_CLASS
 //#define STRING_OPERATIONS
+//#define GEOMETRY
+//#define HARD_CHESS
+#define CHESS
+
 using System;
 using System.CodeDom;
 using System.Collections.Generic;
@@ -13,8 +17,19 @@ namespace IntroductionToDOTNET
 	internal class Program
 	{
 		static readonly string delimiter = "\n-------------------------------------------------------\n";
+
+		const char UPPER_LEFT_ANGLE = '┌';
+		const char UPPER_RIGHT_ANGLE = '┐';
+		const char LOWER_LEFT_ANGLE = '└';
+		const char LOWER_RIGHT_ANGLE = '┘';
+		const char VERTICAL_LINE = '│';
+		const string HORIZONTAL_LINE = "──";
+		const string BLOCK = "██";
+		const string PROBEL = "  ";
+
 		static void Main(string[] args)
 		{
+			Console.SetBufferSize(120, 1000);
 #if CONSOLE_CLASS
 			Console.Title = "Introduction to .NET";
 			Console.WriteLine("\t\t\tHello .NET");
@@ -31,11 +46,11 @@ namespace IntroductionToDOTNET
 			Console.WriteLine("Позиция окна консоли: " + Console.WindowLeft + " знакопозиций");
 			Console.WriteLine("Позиция окна консоли: " + Console.WindowTop + " знакопозиций");
 
-			Console.SetWindowSize(91, 22);
+			Console.SetWindowSize(120, 30);
 			Console.WriteLine("Ширина окна консоли: " + Console.WindowWidth + " знакопозиций");
 			Console.WriteLine("Высота окна консоли: " + Console.WindowHeight + " знакопозиций");
 
-			Console.SetBufferSize(91, 22);
+			Console.SetBufferSize(120, 10000);
 			Console.WriteLine("Ширина буфера консоли: " + Console.BufferWidth + " знакопозиций");
 			Console.WriteLine("Высота буфера консоли: " + Console.BufferHeight + " знакопозиций"); 
 #endif
@@ -55,6 +70,114 @@ namespace IntroductionToDOTNET
 			Console.WriteLine($"{first_name} {last_name} {age}");   //Интерполяция строк  
 #endif
 
+#if GEOMETRY
+			Console.Write("Введите размер фигур: ");
+			int n = Convert.ToInt32(Console.ReadLine());
+
+			//Square
+			for (int i = 0; i < n; i++)
+			{
+				for (int j = 0; j < n; j++)
+					Console.Write(" *");
+				Console.WriteLine();
+			}
+			Console.WriteLine();
+
+			//Triangle 1
+			for (int i = 0; i < n; i++)
+			{
+				for (int j = 0; j <= i; j++)
+					Console.Write(" *");
+				Console.WriteLine();
+			}
+			Console.WriteLine();
+
+			//Triangle 2
+			for (int i = 0; i < n; i++)
+			{
+				for (int j = i; j < n; j++)
+					Console.Write(" *");
+				Console.WriteLine();
+			}
+			Console.WriteLine();
+
+			//Triangle 3
+			for (int i = 0; i < n; i++)
+			{
+				for (int j = 0; j < n; j++)
+					Console.Write(j < i ? "  " : " *");
+				Console.WriteLine();
+			}
+			Console.WriteLine();
+
+			//Triangle 4
+			for (int i = 4; i >= 0; i--)
+			{
+				for (int j = 0; j < n; j++)
+					Console.Write(j >= i ? " *" : "  ");
+				Console.WriteLine();
+			}
+			Console.WriteLine();
+
+			//Rhombus
+			for (int i = 0; i < n; i++)
+			{
+				for (int j = i; j < n; j++) Console.Write(" ");
+				Console.Write("/");
+				for (int j = 0; j < i; j++) Console.Write("  ");
+				Console.WriteLine("\\");
+			}
+			for (int i = 0; i < n; i++)
+			{
+				for (int j = 0; j < i; j++) Console.Write(" ");
+				Console.Write(" \\");
+				for (int j = i + 1; j < n; j++) Console.Write("  ");
+				Console.WriteLine("/");
+			} 
+
+			//Square +-
+			for(int i = 0;i < n; i++)
+			{
+				for (int j = 0; j < n; j++)
+					Console.Write(i % 2 == j % 2 ? "+ " : "- ");
+                Console.WriteLine();
+            }
+#endif
+
+#if HARD_CHESS
+			Console.Write("Введите размер доски: ");
+			int n = Convert.ToInt32(Console.ReadLine());
+
+			for (int i = 0; i < n; i++)
+				for (int j = 0; j < n; j++)
+				{
+					for (int k = 0; k < n; k++)
+						for (int l = 0; l < n; l++)
+							Console.Write(k % 2 == i % 2 ? "* " : "  ");
+					Console.WriteLine();
+				} 
+#endif
+
+#if CHESS
+			Console.Write("Введите размер доски: ");
+			int n = Convert.ToInt32(Console.ReadLine());
+			n++;
+
+			for (int i = 0; i <= n; i++)
+			{
+				for (int j = 0; j <= n; j++)
+				{
+					if (i == 0 && j == 0) Console.Write(UPPER_LEFT_ANGLE);
+					else if (i == 0 && j == n) Console.Write(UPPER_RIGHT_ANGLE);
+					else if (i == n && j == 0) Console.Write(LOWER_LEFT_ANGLE);
+					else if (i == n && j == n) Console.Write(LOWER_RIGHT_ANGLE);
+					else if (i == 0 || i == n) Console.Write(HORIZONTAL_LINE);
+					else if (j == 0 || j == n) Console.Write(VERTICAL_LINE);
+					else Console.Write(i % 2 == j % 2 ? BLOCK : PROBEL);
+				}
+				Console.WriteLine();
+			} 
+#endif
 
 		}
 	}
