081ec709fd1b1e93777864747ed0ee912f7c173d
ec1yps_ <Mihli_sk06@top-academy.ru>
2024-10-07 19:28:33 +0300

    Snake DONE

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,12 +11,43 @@ namespace Snake
 	{
 		static void Main(string[] args)
 		{
-			char key;
+			Console.SetBufferSize(Console.WindowWidth, Console.WindowHeight);
+			Random rand = new Random();
+			int x = rand.Next(Console.WindowWidth);
+			int y = rand.Next(Console.WindowHeight);
+			ConsoleKey key;
+			Console.CursorVisible = false;
 			do
 			{
-				key = Console.ReadKey().KeyChar;
-                Console.WriteLine($"{(int)key}\t{key}");
-            } while(true);
+				Console.Clear();
+
+				Console.SetCursorPosition(0, 0);
+				Console.ResetColor();
+                Console.WriteLine($"X = {x}\tY = {y}");
+
+                Console.SetCursorPosition(x, y);
+				Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine((char)2);
+				key = Console.ReadKey().Key;
+                //Console.WriteLine($"{(int)key}\t{key}");
+				switch (key)
+				{
+					case ConsoleKey.UpArrow:
+					case ConsoleKey.W: y--; break;
+					case ConsoleKey.DownArrow:
+					case ConsoleKey.S: y++; break;
+					case ConsoleKey.LeftArrow:
+					case ConsoleKey.A: x--; break;
+					case ConsoleKey.RightArrow:
+					case ConsoleKey.D: x++; break;
+				}
+
+				if (x == 0 || x == Console.WindowWidth || y == 0 || y == Console.WindowHeight) Console.Beep();
+				if (x == 0) x = 1;
+				if (x == Console.WindowWidth) x = Console.WindowWidth - 1;
+				if (y == 0) y = 1;
+				if(y == Console.WindowHeight) y = Console.WindowHeight - 1;
+            } while(key != ConsoleKey.Escape);
 		}
 	}
 }
